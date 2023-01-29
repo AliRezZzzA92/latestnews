d619b891ea2ccb9e2016bf99b57befafad10cf14
AliRezZzzA92 <75299415+AliRezZzzA92@users.noreply.github.com>
2023-01-29 15:47:03 +0330

    done

--- lib/controller/register_ctr.dart
+++ lib/controller/register_ctr.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: avoid_print
 
-import 'package:blogproject/model/loginmodel.dart' as model;
-import 'package:blogproject/provider/login_state.dart';
 import 'package:blogproject/provider/register_state.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -25,37 +23,37 @@ registerController(String username, String password, String email,
         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
           content: const Text("welcome!"),
           action: SnackBarAction(label: "label", onPressed: () {}),
-          duration: const Duration(seconds: 1),
+          duration: const Duration(seconds: 2),
         ));
 
         // ScaffoldMessenger.of(context).clearSnackBars();
-        // ScaffoldMessenger.of(context).showMaterialBanner(MaterialBanner(
-        //   content: const Text("data"),
-        //   actions: [
-        //     TextButton(
-        //         onPressed: () {
-        //           ScaffoldMessenger.of(context).clearMaterialBanners();
-        //         },
-        //         child: const Text("data")),
-        //   ],
-        // ));
-        // showDialog(
-        //     context: context,
-        //     builder: (context) {
-        //       return AlertDialog(
-        //         title: const Text("salam"),
-        //         actions: [
-        //           IconButton(
-        //               onPressed: () {
-        //                 Navigator.of(context).pop();
-        //               },
-        //               icon: const Icon(Icons.done_all)),
-        //         ],
-        //         shape: RoundedRectangleBorder(
-        //             borderRadius: BorderRadius.circular(15)),
-        //         content: const Text("discription"),
-        //       );
-        //     });
+        ScaffoldMessenger.of(context).showMaterialBanner(MaterialBanner(
+          content: const Text("data"),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).clearMaterialBanners();
+                },
+                child: const Text("data")),
+          ],
+        ));
+        showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: const Text("salam"),
+                actions: [
+                  IconButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      icon: const Icon(Icons.done_all)),
+                ],
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+                content: const Text("discription"),
+              );
+            });
       } else {
         context.read<Registerstate>().changecolor(Colors.redAccent);
 
